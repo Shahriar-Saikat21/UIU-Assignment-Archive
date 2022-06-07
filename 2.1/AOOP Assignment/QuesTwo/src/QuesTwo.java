6eb549e7ed7918b6c43a47f5141193cbd5b4a7bc
SHAHRIAR IMTIAZ SAIKAT <saikatbass@gmail.com>
2022-06-07 12:19:58 +0600

    Delete 2.1/AOOP Assignment directory

--- 2.1/AOOP Assignment/QuesTwo/src/QuesTwo.java
+++
@@ -1,7 +0,0 @@
-public class QuesTwo {
-    public static void main(String[] args) {
-        Container container = new Container();
-        Producer producer = new Producer(container);
-        Consumer consumer = new Consumer(container);
-    }
-}
