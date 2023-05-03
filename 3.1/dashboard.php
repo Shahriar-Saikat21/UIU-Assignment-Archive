198b821812487c996a6125adc58c95415c35094a
SHAHRIAR IMTIAZ SAIKAT <saikatbass@gmail.com>
2023-05-04 00:04:39 +0600

    Add files via upload

---
+++ 3.1/dashboard.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>DBMS Assignment</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"> 
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+    
+    <nav class="navbar bg-body-tertiary navbar">
+        <div class="container">
+          <h1 class="logo">DBMS ASSIGNMENT</h1>
+        </div>
+    </nav>
+
+    <div class="container my-5">
+        <button class="btn btn-primary"><a class="navbar-brand" href="index.html">Log Out</a></button>
+        
+        <form action="showUser.php" method="post" class="mt-3">
+          <button type="submit" name="submit" class="btn btn-primary">Show All Users</button> 
+        </form>       
+    </div>
+
+</body>
+
+</html>
+
+
